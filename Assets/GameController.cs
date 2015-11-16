d51fe46556378c35ff737c71c97253cca0693601
robert schwin <schwin42@gmail.com>
2015-11-16 00:12:54 -0800

    Scenario Instantiation

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GameController : MonoBehaviour {
 
@@ -13,32 +14,43 @@ public class GameController : MonoBehaviour {
 		Incident_Conclusion = 2,
 	}
 
-	Player self = new Player () { playerCharacter = new Character () { name = "Aaron" } };
-	List<Player> connectedPlayers = new List<Player> {
+	public int myCharacterThingId = 0;
+	List<Player> joinedPlayers = new List<Player> {
+		new Player () { playerCharacter = new Character () { name = "Aaron" } },
 		new Player () { playerCharacter = new Character () { name = "Bobby" } },
 		new Player () { playerCharacter = new Character () { name = "Catherine" } },
 		new Player () { playerCharacter = new Character () { name = "Dwayne" } },
 		new Player () { playerCharacter = new Character () { name = "Evan" } },
 		new Player () { playerCharacter = new Character () { name = "Frank" } },
+		new Player () { playerCharacter = new Character () { name = "George" } },
 	};
 
-	public ScreenState _currentState;
+	private ScreenState _currentState = ScreenState.Uninitialized;
 	private Dictionary<ScreenState, GameObject> stateGoReference;
 
-	private Incident currentIncident = null;
+	private InstantiatedIncident currentIncident = null;
+
+	//Game Instance
+	public int nextThingId = 0;
 
 	//UI Elements
 	Transform screenContainer;
 	Text _console;
 	Text _incidentIntro_Prompt;
 	Text _incidentInProgress_Body;
+	Transform _incidentInProgress_ButtonGrid;
+	GameObject _incidentInProgress_ButtonTemplate;
 	Text _incidentConclusion_Resolution;
 
 	// Use this for initialization
 	void Start () {
 	
 		//Temp
-		currentIncident = GameData.incidentRecords[0];
+		currentIncident = GameData.incidentRecords[0].Instantiate(joinedPlayers.Select(p => p.playerCharacter).ToList(), new List<ShipEquipment> { new ShipEquipment { name = "samoflange generator"} });
+		foreach(Player player in joinedPlayers) {
+			player.playerCharacter.Register(nextThingId);
+			nextThingId++;
+		}
 
 		InitializeUi();
 
@@ -52,8 +64,10 @@ public class GameController : MonoBehaviour {
 			//Disable last state and clean up
 			stateGoReference [_currentState].SetActive (false);
 		} else {
+			Debug.Log("Unintialized");
 			//Disable all screens before initialization
 			foreach (KeyValuePair<ScreenState, GameObject> pair in stateGoReference) {
+				Debug.Log("Disabling screen: " + pair.Key);
 				pair.Value.SetActive (false);
 			}
 		}
@@ -65,10 +79,19 @@ public class GameController : MonoBehaviour {
 	private void InitializeState (ScreenState state) {
 		switch(state) {
 		case ScreenState.Incident_Intro:
-			_incidentIntro_Prompt.text = currentIncident.prompt;
+			_incidentIntro_Prompt.text = currentIncident.Prompt;
 			break;
 		case ScreenState.Incident_InProgress:
-			_incidentInProgress_Body.text = currentIncident.beliefs[0].text;
+			_incidentInProgress_Body.text = currentIncident.GetPlayerInfoText(myCharacterThingId);
+			foreach(string s in currentIncident.GetPlayerControlsText(myCharacterThingId)) {
+				GameObject buttonGo = Instantiate<GameObject>(_incidentInProgress_ButtonTemplate) as GameObject;
+				buttonGo.SetActive(true);
+				buttonGo.GetComponentInChildren<Text>().text = s;
+				buttonGo.transform.parent = _incidentInProgress_ButtonGrid;
+				buttonGo.transform.localScale = Vector3.one;
+			}
+//			_incidentInProgress_ButtonGrid.GetComponent<VerticalLayoutGroup>().enabled = true;
+//			_incidentInProgress_Body.text = currentIncident.beliefs[0].text;
 			break;
 		case ScreenState.Incident_Conclusion:
 			_incidentConclusion_Resolution.text = currentIncident.controls[0].conclusionText;
@@ -95,6 +118,15 @@ public class GameController : MonoBehaviour {
 		_console = screenContainer.Find("Console").GetComponent<Text> ();
 		_incidentIntro_Prompt = stateGoReference[ScreenState.Incident_Intro].transform.Find("Prompt").GetComponent<Text> ();
 		_incidentInProgress_Body = stateGoReference[ScreenState.Incident_InProgress].transform.Find("Body").GetComponent<Text> ();
+		_incidentInProgress_ButtonGrid = stateGoReference[ScreenState.Incident_InProgress].transform.Find("ButtonGrid").transform;
+		for(int i = 0; i < _incidentInProgress_ButtonGrid.childCount; i++) {
+			Transform buttonTransform = _incidentInProgress_ButtonGrid.GetChild(i);
+			if(i == 0) {
+				_incidentInProgress_ButtonTemplate = buttonTransform.gameObject;
+				Debug.Log ("Acquired button template: " + _incidentInProgress_ButtonTemplate);
+			}
+			buttonTransform.gameObject.SetActive(false);
+		}
 		_incidentConclusion_Resolution = stateGoReference[ScreenState.Incident_Conclusion].transform.Find("Resolution").GetComponent<Text> ();
 	}
 
--- Assets/Model.cs
+++ Assets/Model.cs
@@ -1,56 +1,165 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Model
 {
+
 }
 
 public class Incident
 {
-	public string prompt;
+	public virtual string Prompt {
+		get {
+			return abstractPrompt;
+		}
+		set {
+			abstractPrompt = value;
+		}
+	}
+
+	public string abstractPrompt;
 	public List<Belief> beliefs;
 	public List<Control> controls;
+
+	public InstantiatedIncident Instantiate (List<Character> charactersById, List<ShipEquipment> shipEquipmentById)
+	{
+		return new InstantiatedIncident (this, charactersById, shipEquipmentById);
+	}
 }
 
-public class InstantiatedIncident : Incident {
+public class InstantiatedIncident : Incident
+{
+	public override string Prompt {
+		get {
+			return InstantiateText (abstractPrompt);
+		}
+	}
 
+	public List<Character> charactersByInternalId;
+	public List<ShipEquipment> shipEquipmentById;
+
+	public InstantiatedIncident (Incident abstractIncident, List<Character> charactersById, List<ShipEquipment> shipEquipmentById)
+	{
+		this.abstractPrompt = abstractIncident.abstractPrompt;
+		this.beliefs = abstractIncident.beliefs;
+		this.controls = abstractIncident.controls;
+
+		this.charactersByInternalId = charactersById;
+		this.shipEquipmentById = shipEquipmentById;
+	}
+
+	public string GetPlayerInfoText (int characterThingId) {
+//		Debug.Log("characters by id count: " + charactersByInternalId.Count);
+//		foreach(Character character in charactersByInternalId) {
+//			Debug.Log("character thing id: " + character.thingId);
+//		}
+//		Debug.Log("belief count: " + beliefs.Count);
+		List<string> infoStrings = this.beliefs
+			.Where(belief => charactersByInternalId[belief.internalCharacterId].thingId == characterThingId)
+			.Select(belief => belief.text).ToList();
+
+//		Debug.Log("info strings count: " + infoStrings.Count());
+		for(int i = 0; i < infoStrings.Count; i++) {
+			infoStrings[i] = InstantiateText(infoStrings[i]);
+		}
+		return string.Join("\n", infoStrings.ToArray());
+	}
+
+	public List<string> GetPlayerControlsText (int characterThingId) {
+		List<string> abstractButtonStrings = this.controls
+			.Where(control => charactersByInternalId[control.internalCharacterId].thingId == characterThingId)
+			.Select(control => control.buttonText).ToList();
+		List<string> instantiatedButtonStrings = new List<string> ();
+		foreach(string s in abstractButtonStrings) {
+			instantiatedButtonStrings.Add(InstantiateText(s));
+		}
+		return instantiatedButtonStrings;
+	}
+
+	private string InstantiateText (string input)
+	{
+		string output = input.Replace ("{", "<");
+		//			Debug.Log("1st version: " + output);
+		int i = 0;
+		while ((i = output.IndexOf("<PC")) != -1) {
+			output = output.Insert (i + 4, "}");
+			//				Debug.Log("1.1 version: " + output);
+			output = output.Remove (i, 3);
+			//				Debug.Log("1.2 version: " + output);
+			output = output.Insert (i, "{");
+			//				Debug.Log("1.3 version: " + output);
+		}
+		//			Debug.Log("character count: " + charactersById.Count);
+		//			Debug.Log("2nd version: " + output);
+		output = string.Format (output, charactersByInternalId.Select (c => c.name).ToArray ());
+		while ((i = output.IndexOf("<SE")) != -1) {
+			output = output.Insert (i + 4, "}");
+			output = output.Remove (i, 3);
+			output = output.Insert (i, "{");
+		}
+		//			Debug.Log("3rd version: " + output);
+		output = string.Format (output, shipEquipmentById.Select (se => se.name).ToArray ());
+		return output;
+
+	}
 }
 
 public class Belief
 {
-	public string relativeCharacterId;
+	public int internalCharacterId;
 	public string text;
 }
 
 public class Control
 {
-	public string relativeCharacterId;
+	public int internalCharacterId;
 	public string buttonText;
 	public List<Outcome> outcomes;
 	public string conclusionText;
 }
 
-public class Outcome {
+public class Outcome
+{
 	public OutcomeCommand command = OutcomeCommand.Uninitialized;
 	public string target;
 
-	public Outcome (OutcomeCommand command, string target) {
+	public Outcome (OutcomeCommand command, string target)
+	{
 		this.command = command;
 		this.target = target;
 	}
 }
 
-public class Character {
+public class Character
+{
+	public int thingId = -1;
+	public string name;
+
+	public Character() {
+
+	}
+	public void Register(int id) {
+		thingId = id;
+	}
+}
+
+public class ShipEquipment
+{
 	public string name;
 }
 
-public class Player {
+[System.Serializable]
+public class Player
+{
 	public Character playerCharacter;
 
 }
 
-public enum OutcomeCommand {
+public enum OutcomeCommand
+{
 	Uninitialized,
 	Destroy,
 }
